e7d692649d8ad3ae9385b10e6712439952436101
Aaron Antrim <aaron@trilliumtransit.com>
2015-09-30 13:21:10 -0700

    generate feature collection of buffers, with agency names

--- geojson.php
+++ geojson.php
@@ -22,46 +22,34 @@ $feeds = array(
 
 // http://gtfs-api.groth-geodata.com/gtfs-api-beta/routes/by-feed/anaheim-ca-us/buffer-meters/400
 
+$buffers = array();
 
-$api_output = file_get_contents('http://gtfs-api.ed-groth.com/gtfs-api/routes/by-feed/eldoradotransit-ca-us'); 
+foreach ($feeds as &$value) {
 
-header('Content-Type: application/json');
+$request_url = $root_api_url.'routes/by-feed/'.$value[1].'/buffer-meters/'.$buffer_distance;
+$api_output = file_get_contents($request_url); 
+
+$properties = array(
+'agency-name' => $value[0],
+'feed-name' => $value[1]
+);
 
 $jsonvar = json_decode($api_output, true);
 
-$route_alignments = Array();
-
-foreach ($jsonvar as &$value) {
-
-$properties = array(
-'name' => $value['route_short_name'],
-'route_short_name' => $value['route_short_name'],
-'route_long_name' => $value['route_long_name'],
-'route_id' => $value['route_id'],
-'route_id' => $value['route_id'],
-'agency_id' => $value['agency_id'],
-'route_color' => $value['route_color']);
-
-if (is_null($value['shared_arcs_geojson'])) 
-	{$geojson = $value['simple_00004_geojson'];}
-else {$geojson = $value['shared_arcs_geojson'];}
-
-
-$geojson = 
+$geojson = $jsonvar[0]['buffer_geojson'];
 
 $new_array = array(
 	'type' => 'Feature',
 	'geometry' => $geojson,
 	'properties' => $properties);
 
-array_push($route_alignments, $new_array);
+array_push($buffers, $new_array);
 
 }
 
 $feature_collection = array(
 	'type' => 'FeatureCollection',
-	'features' => $route_alignments);
+	'features' => $buffers);
 
 echo json_encode ( $feature_collection);
-
 ?>
