0349db532d8fe5bdbccc6376b3ef7c1deb8b755c
Aaron Antrim <aaron@trilliumtransit.com>
2015-09-30 13:05:51 -0700

    beginning the groundwork

--- geojson.php
+++ geojson.php
@@ -1,5 +1,28 @@
 <?php
 
+// in meters
+$buffer_distance = 400;
+$root_api_url = 'http://gtfs-api.groth-geodata.com/gtfs-api-beta/';
+
+// array of feed-ids
+$feeds = array(
+	array("Burbank Bus","burbank-ca-us"),
+	array("FlyAway Bus","laxflyaway-ca-us"),
+//	array("Foothill Transit","foothilltransit-ca-us"),
+//	array("Glendale Beeline","glendale-ca-us"),
+//	array("Los Angeles County Metro","lametro-ca-us"),
+//	array("Metrolink","metrolink-ca-us"),
+	array("Palos Verdes Peninsula Transit Authority","pvpta-ca-us"),
+//	array("Pasadena Area Rapid Transit System","pasadena-ca-us"),
+//	array("Torrance Transit","torrance-ca-us"),
+//	array("Big Blue Bus","santamonica-ca-us"),
+//	array("Culver City Bus","culver-ca-us"),
+//	array("LADOT DASH and Commuter Express","ladash-ca-us")
+	);
+
+// http://gtfs-api.groth-geodata.com/gtfs-api-beta/routes/by-feed/anaheim-ca-us/buffer-meters/400
+
+
 $api_output = file_get_contents('http://gtfs-api.ed-groth.com/gtfs-api/routes/by-feed/eldoradotransit-ca-us'); 
 
 header('Content-Type: application/json');
