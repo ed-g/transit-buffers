bb61f5457f91f815b7c6ba0f00145c3025df5386
Aaron Antrim <aaron@trilliumtransit.com>
2015-09-30 15:13:16 -0700

    /include-stops-buffer

--- geojson.php
+++ geojson.php
@@ -8,25 +8,26 @@ $root_api_url = 'http://gtfs-api.groth-geodata.com/gtfs-api-beta/';
 $feeds = array(
 	array("Burbank Bus","burbank-ca-us"),
 	array("FlyAway Bus","laxflyaway-ca-us"),
-//	array("Foothill Transit","foothilltransit-ca-us"),
-//	array("Glendale Beeline","glendale-ca-us"),
-//	array("Los Angeles County Metro","lametro-ca-us"),
-//	array("Metrolink","metrolink-ca-us"),
+	array("Foothill Transit","foothilltransit-ca-us"),
+	array("Glendale Beeline","glendale-ca-us"),
+	array("Los Angeles County Metro","lametro-ca-us"),
+	array("Metrolink","metrolink-ca-us"),
 	array("Palos Verdes Peninsula Transit Authority","pvpta-ca-us"),
-//	array("Pasadena Area Rapid Transit System","pasadena-ca-us"),
-//	array("Torrance Transit","torrance-ca-us"),
-//	array("Big Blue Bus","santamonica-ca-us"),
-//	array("Culver City Bus","culver-ca-us"),
-//	array("LADOT DASH and Commuter Express","ladash-ca-us")
+	array("Pasadena Area Rapid Transit System","pasadena-ca-us"),
+	array("Torrance Transit","torrance-ca-us"),
+	array("Big Blue Bus","santamonica-ca-us"),
+	array("Culver City Bus","culver-ca-us"),
+	array("LADOT DASH and Commuter Express","ladash-ca-us")
 	);
 
+// example URL
 // http://gtfs-api.groth-geodata.com/gtfs-api-beta/routes/by-feed/anaheim-ca-us/buffer-meters/400
 
 $buffers = array();
 
 foreach ($feeds as &$value) {
 
-$request_url = $root_api_url.'routes/by-feed/'.$value[1].'/buffer-meters/'.$buffer_distance;
+$request_url = $root_api_url.'routes/by-feed/'.$value[1].'/buffer-meters/'.$buffer_distance.'/include-stops-buffer';
 $api_output = file_get_contents($request_url); 
 
 $properties = array(
